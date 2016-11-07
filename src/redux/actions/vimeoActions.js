adf8724f22528ee1549c40f3316f3f348f5e9dac
mshatikhin <mshatikhin@gmail.com>
2016-11-07 16:58:23 +0500

    vimeo

--- src/redux/actions/vimeoActions.js
+++ src/redux/actions/vimeoActions.js
@@ -1,6 +1,7 @@
 // @flow
 
-//const Base64 = require('js-base64').Base64;
+const Base64 = require('js-base64').Base64;
+const vimeoUrl = "https://api.vimeo.com";
 
 export const UPDATE_VIDEO_PORTFOLIO = 'UPDATE_VIDEO_PORTFOLIO';
 
@@ -8,17 +9,29 @@ export function updateVideos(videos: any[]) {
     return { type: UPDATE_VIDEO_PORTFOLIO, videos };
 }
 
+function vimeoAuth(client_id: string, client_secret: string){
+    return fetch(`${vimeoUrl}/oauth/authorize/client?grant_type=client_credentials`, {
+        method: "POST",
+        headers: {
+            "Authorization": "basic " + Base64.encode(client_id + ":" + client_secret)
+        }
+    })
+}
+
 export function videosRequest(userId: string, client_id: string, client_secret: string) {            
     return (dispatch: any) => {
-        return fetch(`https://api.vimeo.com/users/${userId}/videos`,  {
-            headers: {
-                "Authorization" : "Bearer 63e6b08cf575823eecd9ec5209ced4e5"
-            }
-        })
-        .then(response => response.json())
-        .then((response) => {                        
-            return dispatch(updateVideos(response.data));
-        })
-        .catch(({ errors }) => dispatch(updateVideos([])));
+        vimeoAuth(client_id, client_secret)
+            .then(response => response.json())
+            .then(response => {
+            return fetch(`${vimeoUrl}/users/${userId}/videos`,  {
+                headers: {
+                    "Authorization" : "Bearer "+ response.access_token
+                }
+            })
+            .then(response => response.json())
+            .then((response) => {
+                return dispatch(updateVideos(response.data));
+            });
+        }).catch(({ errors }) => dispatch(updateVideos([])));
     };
 }
